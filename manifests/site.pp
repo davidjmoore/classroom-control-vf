63cb4f008c03032f04f79cd53bf7347f7bb9a74c
davidjmoore <davidjmoore@users.noreply.github.com>
2016-08-04 08:43:30 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -62,8 +62,10 @@ package { 'net-tools':
   provider => yum,
 }
 
-yum::group { 'Development Tools':
-  ensure => present,
+exec { 'yum groupinstall Development Tools':
+  command => '/usr/bin/yum -y --disableexcludes=all groupinstall "Development Tools"',
+  unless  => '/usr/bin/yum grouplist "Development Tools" | /bin/grep "^Installed"',
+  timeout => 600 ,
 }
 
 package { 'cowsay':
