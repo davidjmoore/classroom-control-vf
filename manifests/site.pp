b91fb54c2e826f90c6aee47714e27950893168b8
davidjmoore <davidjmoore@users.noreply.github.com>
2016-08-04 08:29:07 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -57,11 +57,15 @@ notify { "Hello, my name is ${::hostname}": }
 # content => "Today I learned what it means to manage state using Puppet.\n",
 #}
 
-package { 'net-tools', 'htop':
+package { 'net-tools':
   ensure => present,
   provider => yum,
 }
 
+package { 'htop':
+  ensure => present,
+  provider => yum,
+}
 
 package { 'cowsay':
   ensure => present,
